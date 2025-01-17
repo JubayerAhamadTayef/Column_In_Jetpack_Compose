4f4c4f0503ddc31170757132af7e1fe64b3bc563
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2025-01-17 17:05:24 +0600

    Practice Column with Spacer!

--- app/src/main/java/com/jubayer_ahamad_tayef/column_in_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/jubayer_ahamad_tayef/column_in_jetpack_compose/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jubayer_ahamad_tayef.column_in_jetpack_compose.ui.theme.Column_In_Jetpack_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +27,60 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column_In_Jetpack_ComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MyColumn(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun MyColumn(modifier: Modifier) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(5.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(MaterialTheme.colorScheme.primary)
+            )
+            Spacer(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(MaterialTheme.colorScheme.primary)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Spacer(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(MaterialTheme.colorScheme.primary)
+                    .align(Alignment.End)
+            )
+            Spacer(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(MaterialTheme.colorScheme.primary)
+                    .align(Alignment.End)
+            )
+            Spacer(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(MaterialTheme.colorScheme.primary)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Spacer(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(MaterialTheme.colorScheme.primary)
+            )
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Column_In_Jetpack_ComposeTheme {
-        Greeting("Android")
+    @Preview(showSystemUi = true)
+    @Composable
+    fun MyColumnPreview() {
+        MyColumn(Modifier.padding(5.dp))
     }
 }
